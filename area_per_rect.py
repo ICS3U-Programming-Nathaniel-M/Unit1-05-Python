9fdade69a82b8cfb1cb3f1e048c8abe580684bb0
Nathaniel Mosazgi <nathaniel.mosazgi@stu.ocsb.ca>
2025-10-02 18:07:18 +0000

    added area_per-rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Created By: Nathaniel
+# Date: October 2, 2025
+# Write what this program does.
+
+
+def main():
+    print("For a rectangle with length 7cm and width 4cm.")
+    print("The area is {}cm^2".format(7 * 4))
+
+
+if __name__ == "__main__":
+    main()
